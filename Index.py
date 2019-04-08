f92e42a70b6c8f16242f143b29dcb831adc2bd03
HackerMaestro <48749813+HackerMaestro@users.noreply.github.com>
2019-04-08 18:57:57 +0100

    Defining new functions as well as declaring new variables

--- Index.py
+++ Index.py
@@ -1,1 +1,16 @@
-import os, sys
+import os, sys, tkinter
+
+#Declaring variables
+currentDirectory = os.getcwd()
+
+#Defining functions
+def makeDirectory(path, name):
+    os.chdir(path)
+    os.mkdir(name)
+
+def getCurrentDirectory():
+    print(currentDirectory)
+
+def changeDirectory(path):
+    os.chdir(path)
+
