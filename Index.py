869395543c2fd54e64ad7ae59286cdb3f3ed7488
HackerMaestro <48749813+HackerMaestro@users.noreply.github.com>
2019-04-15 12:00:47 +0100

    Update Index.py (version 2.0)
    Now has some commands and a prompt for the user (until they type in exit).

--- Index.py
+++ Index.py
@@ -1,16 +1,39 @@
-import os, sys, tkinter
+import os, socket
 
-#Declaring variables
-currentDirectory = os.getcwd()
+"""Creating a socket for network based commands"""
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Defining functions
-def makeDirectory(path, name):
-    os.chdir(path)
-    os.mkdir(name)
+"""Defining functions"""
+def ipAddress(website):
+  try:
+    print(socket.gethostbyname(website))
+  except:
+    print("The webaddress which you entered was invalid.")
+    
+def pwd():
+  print(os.getcwd())
+  
+def cd(path):
+  os.chdir(path)
+    
+"""Programming the event loop"""
+running = True
 
-def getCurrentDirectory():
-    print(currentDirectory)
-
-def changeDirectory(path):
-    os.chdir(path)
-
+while running:
+  user_input = input(">>>$")
+  if user_input == "getIPAddress":
+    webaddress = input(">>>$")
+    ipAddress(webaddress)
+  elif user_input == "pwd":
+    pwd()
+  elif user_input == "cd":
+    path = input(">>>$")
+    pathExists = os.path.exists(path)
+    if pathExists:
+      cd(path)
+    else:
+      print("That path doesn't exist")
+  elif user_input == "exit":
+    running = False
+  else:
+    print("The command which you entered was invalid.")
